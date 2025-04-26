ddc34d1a9e1dc8d9a518121a4d77070cfc932888
ADhal99 <abinashdhal48@gmail.com>
2025-04-26 12:09:40 +0530

    Initial commit Spring Boot app setup for queue listner

---
+++ src/main/java/com/abd/azure/queue_listner/QueueListnerApplication.java
@@ -0,0 +1,13 @@
+package com.abd.azure.queue_listner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueListnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueListnerApplication.class, args);
+	}
+
+}
